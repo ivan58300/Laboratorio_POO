931ca63f8c13b0b71c4675819bbe59ce8553e0cc
ivan58300 <59028802+ivan58300@users.noreply.github.com>
2021-11-13 17:04:51 -0600

    Add files via upload

---
+++ Practica9/src/practica9/Practica9.java
@@ -0,0 +1,15 @@
+
+package practica9;
+
+
+
+public class Practica9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
